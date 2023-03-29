72f455128c8432fbcf4e95faf4f74c62d9ecb807
Domain835 <domain835@gmail.com>
2023-03-29 19:42:23 +0600

    Addув models mapping

--- Notes.Application/Interfaces/INotesDbContext.cs
+++ Notes.Application/Interfaces/INotesDbContext.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
 
 namespace Notes.Application.Interfaces
 {
     public interface INotesDbContext
     {
-
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
--- Notes.Persistance/DbInitializer.cs
+++ Notes.Persistance/DbInitializer.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Notes.Persistance
+﻿namespace Notes.Persistance
 {
     public class DbInitializer
     {
-
+        public static void Initialize(NotesDbContext context)
+        { 
+            context.Database.EnsureCreated();
+        }
     }
 }
